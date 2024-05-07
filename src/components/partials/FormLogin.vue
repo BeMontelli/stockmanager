486e59f6aed50432b427e4ca2c439b48ebab4e92
benjaminmontellimard <benjamin.m@coqpit.fr>
2024-05-07 11:49:32 +0200

    Added login form

--- src/components/partials/FormLogin.vue
+++ src/components/partials/FormLogin.vue
@@ -2,30 +2,42 @@
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
 
-// const welcomeMessage = ref('');
-//
-// onMounted(() => {
-//   fetchWelcomeMessage();
-// });
-//
-// const fetchWelcomeMessage = () => {
-//   axios.get('http://127.0.0.1:8000/api/v1/welcome')
-//       .then(response => {
-//         console.log(response.data.title);
-//         welcomeMessage.value = response.data.title;
-//       })
-//       .catch(error => {
-//         console.error('Error fetching welcome message:', error);
-//       });
-// };
+const formData = ref({
+  email: '',
+  password: ''
+});
+const error = ref('');
+
+const login = async () => {
+  try {
+    const response = await axios.post('http://127.0.0.1:8000/api/v1/login', formData.value);
+    // Handle successful login
+    console.log(response);
+    console.log('Login successful');
+  } catch (err) {
+    error.value = 'Invalid username or password';
+    console.error('Error logging in:', err);
+  }
+};
 </script>
 
 <template>
-  <section class="page__login_form">
-
-  </section>
+  <form @submit.prevent="login">
+    <div>
+      <label for="email">Email:</label>
+      <input type="text" id="email" v-model="formData.email">
+    </div>
+    <div>
+      <label for="password">Password:</label>
+      <input type="password" id="password" v-model="formData.password">
+    </div>
+    <button type="submit">Login</button>
+  </form>
+  <p v-if="error" class="error">{{ error }}</p>
 </template>
 
-<style scoped>
-
+<style>
+.error {
+  color: red;
+}
 </style>
