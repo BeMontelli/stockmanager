55e087db0355b1d370b9a257c1e0f6470891dedf
benjaminmontellimard <benjamin.m@coqpit.fr>
2024-05-14 11:04:48 +0200

    Added page 404

--- src/router/index.js
+++ src/router/index.js
@@ -6,10 +6,15 @@ import LogoutView from '../views/LogoutView.vue'
 import RegisterView from '../views/RegisterView.vue'
 import ProfileView from '../views/ProfileView.vue'
 import ProductCreate from '../views/ProductCreate.vue'
+import NotFoundView from '../views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/:pathMatch(.*)*',
+      component: NotFoundView
+    },
     {
       path: '/',
       name: 'home',
